c40836bfcd3ec8dccbeec0dd9b3159cd2f923d54
daanreede <93087304+Daan-Reede@users.noreply.github.com>
2022-10-06 20:54:45 +0200

    p6 config

---
+++ src/main/java/model/Reiziger.java
@@ -0,0 +1,89 @@
+package model;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reiziger {
+    private int id;
+    private String voorletters;
+    private String tussenvoegsel;
+    private String achternaam;
+    private Date geboortedatum;
+    private Adres adres;
+    private List<OVChipkaart> OVChipkaarten = new ArrayList<>();
+
+    public Reiziger(int id, String voorletters, String tussenvoegsel, String achternaam, Date geboortedatum){
+        this.id = id;
+        this.voorletters = voorletters;
+        this.tussenvoegsel = tussenvoegsel;
+        this.achternaam = achternaam;
+        this.geboortedatum = geboortedatum;
+        this.adres = null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getVoorletters() {
+        return voorletters;
+    }
+
+    public String getTussenvoegsel() {
+        return tussenvoegsel;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public Date getGeboortedatum() {
+        return geboortedatum;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setVoorletters(String voorletters) {
+        this.voorletters = voorletters;
+    }
+
+    public void setTussenvoegsel(String tussenvoegsel) {
+        this.tussenvoegsel = tussenvoegsel;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public void setGeboortedatum(Date geboortedatum) {
+        this.geboortedatum = geboortedatum;
+    }
+
+    public void setAdres(Adres adres) {
+        this.adres = adres;
+    }
+
+    public Adres getAdres() {
+        return adres;
+    }
+
+    public List<OVChipkaart> getOVChipkaarten() {
+        return OVChipkaarten;
+    }
+
+    public void setOVChipkaarten(List<OVChipkaart> OVChipkaarten) {
+        this.OVChipkaarten = OVChipkaarten;
+    }
+
+    public void addOVChipkaart(OVChipkaart ov){
+        this.OVChipkaarten.add(ov);
+    }
+
+    @Override
+    public String toString() {
+        return "Reiziger " + this.getVoorletters() + "." + (this.getTussenvoegsel() != null ? this.getTussenvoegsel() : "") + " " + this.getAchternaam();
+    }
+}
